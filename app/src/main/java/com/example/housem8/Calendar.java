b5dfc00b7310e50583a984f9c0fbe47a8399f623
jjfrl <jlinley28@gmail.com>
2021-03-16 17:36:39 +0000

    added functionality to navigate from Main Activity to Calendar Fragment

--- app/src/main/java/com/example/housem8/Calendar.java
+++ app/src/main/java/com/example/housem8/Calendar.java
@@ -1,5 +1,6 @@
 package com.example.housem8;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -16,19 +17,35 @@ import android.widget.Button;
  */
 public class Calendar extends Fragment {
 
-    Button testButton;
+    Button toMainButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View v = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        toMainButton = v.findViewById(R.id.toMainButton);
+        toMainButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MainActivity.class));
+            }
+        });
+
+        return v;
+
+    }
+
+    public void toolBar(){
+
     }
 
 
--- app/src/main/java/com/example/housem8/MainActivity.java
+++ app/src/main/java/com/example/housem8/MainActivity.java
@@ -2,21 +2,51 @@ package com.example.housem8;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModel;
 import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button pressButton;
+    Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolBar();
+        calFrag();
 
+    }
+
+    //method to show toolbar on each page
+    public void toolBar(){
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+
+    //method to change to fragment once button is clicked
+    public void calFrag(){
+        testButton = findViewById(R.id.testButton);
+        
+        testButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                Calendar cal = new Calendar();
+                FragmentManager manager = getSupportFragmentManager();
+                manager.beginTransaction().add(R.id.mainLayout, cal).commit();
+            }
+        });
+
+        
+
+
+    }
+
+
+
 }
