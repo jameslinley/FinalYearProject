1fbc0b49d926cd8216fb51e922ba5a812fd37347
jjfrl <jlinley28@gmail.com>
2021-03-20 19:30:40 +0000

    added firebase and more functionality to Messages

--- app/src/main/java/com/example/housem8/MessagesActivity.java
+++ app/src/main/java/com/example/housem8/MessagesActivity.java
@@ -7,16 +7,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MessagesActivity extends AppCompatActivity {
 
     private Button toMainButton;
+    private EditText sendMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
         toMainButton = findViewById(R.id.toMain);
+        sendMessage = findViewById(R.id.enterMessage);
 
         toolBar();
         toMain();
